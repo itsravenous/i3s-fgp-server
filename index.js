37d7f55ce5068dbb7dc79a28348fe3cdf126aa69
Tom Jenkins <tom@itsravenous.com>
2015-01-20 23:39:13 +0000

    - Separating out db util functions into module - Adding link to animal resource in fgp objects

--- index.js
+++ index.js
@@ -3,66 +3,34 @@ var http = require('http'),
 	url = require('url'),
 	fs = require('fs'),
 	path = require('path'),
-	fgpReader = require('i3s-fgp-reader'),
+	dbUtils = require('i3s-db-utils'),
 	config = require('./config');
-
-var fgpDir = config.fgpDir;
-
-/**
- * Provides the image filename corresponding to a fingerprint
- * @param {String} fingerprint filename
- * @return {String} image filename
- */
-function getFGPImageName (fgpFile) {
-	return fgpFile.split('.fgp')[0]+'.jpg';
-}
-
-/**
- * Fetches fingerprints for an individual
- * @param {String} individual ID
- * @return {Array}
- */
-function getFingerprintsForIndividual(id) {
-	var dir = path.join(config.fgpDir, id);
-	if (!fs.existsSync(dir) || !fs.statSync(dir).isDirectory()) {
-		return false;
-	}
-
-	var fgpFiles = fs.readdirSync(dir).filter(function (entry) {
-		return entry.split('.').pop().toLowerCase() == 'fgp';
-	});
-	var fgps = [];
-	fgpFiles.forEach(function (fgpFile) {
-		fgpFile = path.join(dir, fgpFile);
-		var fgpData = fgpReader(fgpFile);
-		fgps.push({
-			fgp: fgpData,
-			img64: fs.readFileSync(getFGPImageName(fgpFile)).toString('base64')
-		});
-	});
-	return fgps;
-}
-
-/**
- * Fetches fingerprints for all individuals
- * @return {Array}
- */
-function getAllFingerprints() {
-	var ids = fs.readdirSync(config.fgpDir);
-	var fgps = ids.map(getFingerprintsForIndividual).filter(function (id) { return id});
-	return fgps;
-}
 
 // Route requests
 server.on('request', function (req, res) {
+
+	var addFGPLinks = function (fgp) {
+		fgp.links = [
+			{
+				rel: 'animal',
+				url: req.headers.host + reqPath
+			}
+		]
+		return fgp;
+	}
+
 	var url_parts = url.parse(req.url);
 	var reqPath = decodeURIComponent(url_parts.path);
 
 	if (reqPath == '/') {
 		// Fetch fingerprints for all individuals
-		var result = getAllFingerprints();
+		var result = dbUtils.getAllFingerprints(config.fgpDir);
+		result = [].concat.apply(result);
+		result.map(addFGPLinks);
 	} else {
-		var result = getFingerprintsForIndividual(reqPath);
+		// Fetch fingerprints for an individual
+		var result = dbUtils.getFingerprintsForIndividual(path.join(config.fgpDir, reqPath));
+		result = result.map(addFGPLinks);
 	}
 
 	res.end(JSON.stringify(result));	
